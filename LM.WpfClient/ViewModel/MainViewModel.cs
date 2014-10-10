6671e729a4c6eb38dfcccb64df7f772684741bfb
saurabh-singh-cardinalts <saurabh.singh@cardinalts.com>
2014-10-10 16:33:28 +0530

    Design updates

--- LM.WpfClient/ViewModel/MainViewModel.cs
+++ LM.WpfClient/ViewModel/MainViewModel.cs
@@ -1,15 +1,23 @@
+using System;
+using System.Collections.Generic;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using System.ComponentModel;
+using System.Timers;
+using System.Windows.Threading;
 
 namespace LM.WpfClient.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
         private string _navigationSource;
+        private DateTime _autoTimer;
 
         public ICommand AddEmployee { get; internal set; }
         public ICommand EditEmployee { get; internal set; }
+
+        Dictionary<string, string> navSource;
 
         public string NavigationSource
         {
@@ -21,10 +29,30 @@ namespace LM.WpfClient.ViewModel
             }
         }
 
+        public DateTime AutoTimer
+        {
+            get { return _autoTimer; }
+            set
+            {
+                _autoTimer = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public MainViewModel()
         {
             AddEmployee = new RelayCommand(AddEmployeeExecute);
-            EditEmployee = new RelayCommand(EditEmployeeExecute); 
+            EditEmployee = new RelayCommand(EditEmployeeExecute);
+            navSource = new Dictionary<string, string>();
+
+            navSource.Add("AddEmployee", "Views/AddEmployee.xaml");
+            navSource.Add("EditEmployee", "Views/EditEmployee.xaml");
+
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
 
             ////if (IsInDesignMode)
             ////{
@@ -36,13 +64,46 @@ namespace LM.WpfClient.ViewModel
             ////}
         }
 
+        void timer_Tick(object sender, EventArgs e)
+        {
+            AutoTimer = DateTime.Now;
+        }
+
         private async void AddEmployeeExecute()
         {
-            NavigationSource = "Views/AddEmployee.xaml";
+            NavigationSource = navSource["AddEmployee"];
         }
         private async void EditEmployeeExecute()
         {
-            NavigationSource = "Views/EditEmployee.xaml";
+            NavigationSource = navSource["EditEmployee"];
         }
+
+
     }
+
+
+    public class Ticker : INotifyPropertyChanged
+    {
+        public Ticker()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000; // 1 second updates
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+
+        public DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs("Now"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
 }
